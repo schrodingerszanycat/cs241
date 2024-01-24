f7f047d12a1877f54ed68056bb2e6ee4598dd360
schrodingerszanycat <152669825+thebardofavon@users.noreply.github.com>
2024-01-24 22:13:52 +0530

    Update queries.sql

--- assignment_3/queries.sql
+++ assignment_3/queries.sql
@@ -1,4 +1,5 @@
--- mysql> SELECT DISTINCT(fname) FROM faculty;
+-- Question 1
+SELECT DISTINCT(fname) FROM faculty;
 -- +--------------+
 -- | fname        |
 -- +--------------+
@@ -14,7 +15,8 @@
 -- +--------------+
 -- 9 rows in set (0.00 sec)
 
--- mysql> SELECT DISTINCT(dname) FROM department;
+-- Question 2
+SELECT DISTINCT(dname) FROM department;
 -- +------------------+
 -- | dname            |
 -- +------------------+
@@ -29,7 +31,8 @@
 -- +------------------+
 -- 8 rows in set (0.00 sec)
 
--- mysql> SELECT fname FROM faculty AS f, teaches AS t WHERE f.fid = t.fid AND t.cid = 1;
+-- Question 3
+SELECT fname FROM faculty AS f, teaches AS t WHERE f.fid = t.fid AND t.cid = 1;
 -- +-------------+
 -- | fname       |
 -- +-------------+
@@ -38,7 +41,8 @@
 -- +-------------+
 -- 2 rows in set (0.00 sec)
 
--- mysql> SELECT sname FROM student AS s, takes AS t WHERE s.sid = t.sid GROUP BY sname;
+-- Question 4
+SELECT sname FROM student AS s, takes AS t WHERE s.sid = t.sid GROUP BY sname;
 -- +----------------+
 -- | sname          |
 -- +----------------+
@@ -65,7 +69,8 @@
 -- +----------------+
 -- 20 rows in set (0.00 sec)
 
--- mysql> SELECT sname FROM student AS s, takes AS t WHERE s.sid = t.sid GROUP BY sname HAVING COUNT(cid) > 1;
+-- Question 5
+SELECT sname FROM student AS s, takes AS t WHERE s.sid = t.sid GROUP BY sname HAVING COUNT(cid) > 1;
 -- +--------------+
 -- | sname        |
 -- +--------------+
@@ -77,10 +82,12 @@
 -- +--------------+
 -- 5 rows in set (0.00 sec)
 
--- mysql> SELECT sname FROM student AS s, takes AS t WHERE s.sid = t.sid GROUP BY sname HAVING COUNT(cid) = 3;
+-- Question 6
+SELECT sname FROM student AS s, takes AS t WHERE s.sid = t.sid GROUP BY sname HAVING COUNT(cid) = 3;
 -- Empty set (0.00 sec)
 
--- mysql> SELECT sname FROM student AS s WHERE s.sid NOT IN (SELECT takes.sid FROM takes);
+-- Question 7
+SELECT sname FROM student AS s WHERE s.sid NOT IN (SELECT takes.sid FROM takes);
 -- +-------------------+
 -- | sname             |
 -- +-------------------+
@@ -88,9 +95,8 @@
 -- +-------------------+
 -- 1 row in set (0.00 sec)
 
--- mysql> SELECT *
---     -> FROM department
---     -> WHERE budget = (SELECT MAX(budget) FROM department);
+-- Question 8
+SELECT * FROM department WHERE budget = (SELECT MAX(budget) FROM department);
 -- +-----+-------------+----------+-----------+
 -- | did | dname       | building | budget    |
 -- +-----+-------------+----------+-----------+
@@ -98,14 +104,31 @@
 -- +-----+-------------+----------+-----------+
 -- 1 row in set (0.00 sec)
 
+-- Question 9
+WITH fac_count (fid, fname, course_count) 
+    AS (SELECT t.fid, f.fname, count(cid) 
+    FROM teaches AS t, faculty AS f WHERE f.fid = t.fid 
+    GROUP BY fid ORDER BY count(cid) ASC) 
+SELECT fname FROM fac_count WHERE course_count = (SELECT MAX(course_count) FROM fac_count);
+-- +-------------+
+-- | fname       |
+-- +-------------+
+-- | Dr Sailesh  |
+-- | Dr Mohan    |
+-- | Dr Priyanka |
+-- +-------------+
+-- 3 rows in set (0.00 sec)
 
-WITH faculty_course_count AS (
-    SELECT f.fname, COUNT(t.cid) AS subject_count
-    FROM faculty AS f, teaches AS t
-    WHERE t.fid = f.fid
-    GROUP BY f.fname
-)
-
-SELECT ALL fname 
-FROM faculty_course_count
-WHERE subject_count = (SELECT MAX(subject_count) FROM faculty_course_count);
+-- Question 10
+WITH tab (building, dept_count) 
+    AS (SELECT building, count(dname) 
+    FROM department 
+    GROUP BY building 
+    ORDER BY COUNT(dname) ASC)
+SELECT building FROM tab WHERE dept_count = (SELECT MAX(dept_count) FROM tab);
+-- +----------+
+-- | building |
+-- +----------+
+-- | A        |
+-- +----------+
+-- 1 row in set (0.00 sec)
