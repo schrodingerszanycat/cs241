6d03a23604b011690e75ba8c20abf3bade2f4af4
schrodingerszanycat <152669825+thebardofavon@users.noreply.github.com>
2024-02-02 11:52:26 +0530

    Update

--- assignment_4/queries.sql
+++ assignment_4/queries.sql
@@ -1,21 +1,9 @@
--- Question 1
 SELECT COUNT(*) FROM course;
--- +----------+
--- | COUNT(*) |
--- +----------+
--- |       10 |
--- +----------+
--- 1 row in set (0.01 sec)
+
 
 WITH course_count (cid, student_count) 
     AS (SELECT cid, COUNT(*) FROM takes GROUP BY cid HAVING COUNT(*) > 2) 
     SELECT COUNT(cid) FROM course_count;
--- +------------+
--- | COUNT(cid) |
--- +------------+
--- |          5 |
--- +------------+
--- 1 row in set (0.00 sec)
 
 
 WITH course_count (cid, student_count) 
@@ -24,8 +12,29 @@ WITH course_count (cid, student_count)
     WHERE student_count = (SELECT MAX(student_count) FROM course_count);
 
 
-SELECT f.fid, f.fname, t.cid FROM faculty AS f, teaches AS t WHERE t.fid = f.fid;
-
 WITH course_count (fid, fname, c_count) 
-    AS (SELECT f.fid, f.fname, t.cid FROM faculty AS f, teaches AS t WHERE t.fid = f.fid AND GROUP BY cid HAVING COUNT(*) > 2)
-    SELECT fname FROM course_count;
+    AS (SELECT f.fid, f.fname, COUNT(*) 
+    FROM faculty AS f, teaches AS t
+    WHERE t.fid = f.fid 
+    GROUP BY f.fid, f.fname HAVING COUNT(*) > 2)
+SELECT fid, fname FROM course_count;
+
+
+WITH student_count (fid, fname, s_count)
+    AS (SELECT f.fid, f.fname, COUNT(*)
+    FROM advisor AS a, faculty AS f
+    WHERE f.fid = a.fid
+    GROUP BY fid, fname 
+    HAVING COUNT(*) > 4)
+SELECT fname FROM student_count;
+
+
+
+
+
+WITH fac_info (fid, fname, cid, credit_count) 
+    AS (SELECT t.fid, f.fname, c.cid, c.credit
+    FROM teaches AS t, faculty AS f, course AS C
+    WHERE c.cid = t.cid AND t.fid = f.fid
+    )
+SELECT * FROM fac_info;
