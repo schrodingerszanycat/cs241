82cf5c0feb7c9bd479196bcf717b9897652a8762
schrodingerszanycat <152669825+thebardofavon@users.noreply.github.com>
2024-02-09 09:10:22 +0530

    Update queries.sql

--- assignment_4/queries.sql
+++ assignment_4/queries.sql
@@ -1,18 +1,19 @@
-----------1
+USE AS3;
+-- --------1
 SELECT COUNT(*) FROM course;
 
-----------2
+-- --------2
 WITH course_count (cid, student_count) AS (
     SELECT cid, COUNT(*) FROM takes GROUP BY cid HAVING COUNT(*) > 2) 
     SELECT COUNT(*) FROM course_count;
 
-----------3
+-- --------3
 WITH course_count (cid, student_count) AS (
     SELECT cid, COUNT(sid) FROM takes GROUP BY cid) 
     SELECT cid, student_count FROM course_count 
     WHERE student_count = (SELECT MAX(student_count) FROM course_count);
 
-----------4
+-- --------4
 WITH course_count (fid, fname, c_count) AS (
     SELECT f.fid, f.fname, COUNT(*) 
     FROM faculty AS f, teaches AS t
@@ -20,7 +21,7 @@ WITH course_count (fid, fname, c_count) AS (
     GROUP BY f.fid, f.fname HAVING COUNT(*) > 2)
 SELECT fid, fname FROM course_count;
 
-----------5
+-- --------5
 WITH student_count (fid, fname, s_count) AS (
     SELECT f.fid, f.fname, COUNT(*)
     FROM advisor AS a, faculty AS f
@@ -29,7 +30,7 @@ WITH student_count (fid, fname, s_count) AS (
     HAVING COUNT(*) > 4)
 SELECT fname FROM student_count;
 
-----------6
+-- --------6
 WITH fac_info (fid, fname, cid, credits) AS (
     SELECT t.fid, f.fname, c.cid, c.credit
     FROM teaches AS t, faculty AS f, course AS c
@@ -37,7 +38,7 @@ WITH fac_info (fid, fname, cid, credits) AS (
     )
 SELECT fid, fname FROM fac_info WHERE credits = (SELECT MAX(credits) FROM fac_info) GROUP BY fid;
 
-----------7
+-- --------7
 WITH credit_info (fid, fname, total_credits) AS (
     SELECT t.fid, f.fname, SUM(credit)
     FROM teaches AS t, course AS c, faculty AS f
@@ -46,10 +47,10 @@ WITH credit_info (fid, fname, total_credits) AS (
     )
 SELECT fname FROM credit_info WHERE total_credits = (SELECT MAX(total_credits) FROM credit_info);
 
------------8
-NULL
+-- ---------8
+-- NULL
 
------------9
+-- ---------9
 WITH fac_info (fid, fname, stud_count) AS (
     SELECT a.fid, f.fname, count(*) 
     FROM faculty AS f, advisor AS a
@@ -59,7 +60,7 @@ WITH fac_info (fid, fname, stud_count) AS (
 
 SELECT * FROM fac_info WHERE stud_count = (SELECT MAX(stud_count) FROM fac_info);
 
------------10 
+-- ---------10 
 -- (What is the difference between these 2 queries?)
 
 -- Query number 1
@@ -78,14 +79,14 @@ WHERE s.sid = t.sid AND t.cid = c.cid
 GROUP BY s.sid, s.sname 
 HAVING COUNT(c.did) >= 2;
 
-----------11
+-- --------11
 SELECT s.sname 
 FROM student s, takes t, course c 
 WHERE s.sid = t.sid AND t.cid = c.cid 
 GROUP BY s.sid, s.sname 
 HAVING COUNT(c.did) = 1;
 
----------12
+-- -------12
 
 -- SELECT ta.sid, ta.cid, te.fid AS teaching_faculty, ad.fid AS advising_faculty 
 -- FROM takes ta, teaches te, advisor ad WHERE ta.cid = te.cid AND ad.sid = ta.sid;
@@ -108,7 +109,7 @@ WITH student_info (sid, sname, teaching_faculty, advising_faculty) AS (
 
 SELECT DISTINCT(sname) FROM student_info WHERE teaching_faculty = advising_faculty;
 
------------13
+-- ---------13
 WITH student_info (sid, sname, teaching_faculty, advising_faculty) AS (
     SELECT ta.sid, st.sname, te.fid AS teaching_faculty, ad.fid AS advising_faculty 
     FROM takes ta, student st, teaches te, advisor ad 
@@ -122,7 +123,7 @@ SELECT DISTINCT(sname) FROM student_info
         WHERE teaching_faculty = advising_faculty
     );
 
------------14
+-- ---------14
 -- SELECT t.sid, c.cid, d.did FROM takes t, course c, department d WHERE t.cid = c.cid AND c.did = d.did;
 
 -- SELECT t.sid, d.did FROM takes t, course c, department d WHERE t.cid = c.cid AND c.did = d.did;
@@ -138,7 +139,7 @@ WITH department_info (did, dname, total_students) AS (
 
 SELECT dname FROM department_info WHERE total_students = (SELECT MAX(total_students) FROM department_info);
 
------------15
+-- ---------15
 -- SELECT t.fid, d.did, d.dname FROM teaches t, course c, department d WHERE t.cid = c.cid AND c.did = d.did;
 
 WITH department_info (did, dname, total_faculty) AS (
@@ -150,7 +151,7 @@ WITH department_info (did, dname, total_faculty) AS (
 
 SELECT dname FROM department_info WHERE total_faculty = (SELECT MAX(total_faculty) FROM department_info);
 
-------------16
+-- ----------16
 -- WITH alias AS (
 --     SELECT t.fid, t.cid, c.cname, d.did, d.dname 
 --     FROM teaches t, course c, department d
@@ -166,28 +167,28 @@ WITH course_info AS (
 
 SELECT t1.cname course1, t2.cname course2 FROM course_info t1, course_info t2 WHERE t1.fid = t2.fid AND t1.cid < t2.cid;
 
--------------17
+-- -----------17
 SELECT s.sname FROM student s, takes t1, takes t2 
 WHERE s.sid = t1.sid    
   AND s.sid = t2.sid 
   AND t1.cid = 1 
   AND t2.cid = 2;
 
--------------18
+-- -----------18
 WITH department_info AS (
     SELECT f.did, d.dname, AVG(f.salary) AS avg_salary FROM faculty f, department d WHERE f.did = d.did GROUP BY f.did, d.dname
 )
 
 SELECT dname FROM department_info WHERE avg_salary = (SELECT MAX(avg_salary) FROM department_info);
 
-------------19
+-- ----------19
 WITH department_info AS (
     SELECT f.did, d.dname, AVG(f.salary) AS avg_salary FROM faculty f, department d WHERE f.did = d.did GROUP BY f.did, d.dname
 )
 
 SELECT dname FROM department_info WHERE avg_salary = (SELECT MIN(avg_salary) FROM department_info);
 
-------------20
+-- ----------20
 WITH dept_info AS ( 
     SELECT s.age, d.dname FROM student s, department d WHERE s.did = d.did
 )
