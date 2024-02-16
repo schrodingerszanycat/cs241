02803d7db79219f212a38e816aa1a02921f343bb
schrodingerszanycat <152669825+thebardofavon@users.noreply.github.com>
2024-02-16 09:08:03 +0530

    Update queries.sql

--- assignment__5/queries.sql
+++ assignment__5/queries.sql
@@ -38,11 +38,10 @@ WHERE credit = (SELECT MAX(credit) FROM course WHERE credit NOT IN (SELECT MAX(c
 
 -- Alternate query
 
--- SELECT cname FROM course 
--- WHERE credit = (
---     SELECT DISTINCT credit FROM course t1 
---     WHERE (SELECT COUNT(DISTINCT credit) FROM course t2 WHERE t2.credit < t1.credit) = 1
--- );
+SELECT cname FROM course 
+WHERE credit = (
+    SELECT DISTINCT credit FROM course t1 WHERE (SELECT COUNT(DISTINCT credit) FROM course t2 WHERE t2.credit < t1.credit) = 1
+);
 
 -- 6
 SELECT f.fname, a.title FROM faculty f, advisor a WHERE f.fid = a.fid AND a.title LIKE '%AI%';
@@ -51,7 +50,55 @@ SELECT f.fname, a.title FROM faculty f, advisor a WHERE f.fid = a.fid AND a.titl
 SELECT f.fname FROM faculty f WHERE f.fid NOT IN (SELECT t.fid FROM teaches t);
 
 -- 8
-SELECT * FROM faculty f, course c WHERE f.did = c.did;
+SELECT faculty.fid, faculty.fname
+FROM faculty
+WHERE NOT EXISTS (
+    SELECT course.cid FROM course, teaches
+    WHERE faculty.fid = teaches.fid AND course.cid = teaches.cid
+    AND course.did = faculty.did
+) AND EXISTS (
+    SELECT course.cid FROM course, teaches
+    WHERE faculty.fid = teaches.fid AND course.cid = teaches.cid
+    AND course.did != faculty.did
+)
+UNION
+SELECT faculty.fid, faculty.fname
+FROM faculty
+WHERE NOT EXISTS (
+    SELECT course.cid FROM course, teaches
+    WHERE faculty.fid = teaches.fid AND course.cid = teaches.cid
+    AND course.did != faculty.did
+) AND EXISTS (
+    SELECT course.cid FROM course, teaches
+    WHERE faculty.fid = teaches.fid AND course.cid = teaches.cid
+    AND course.did = faculty.did
+);
+
+-- 9
+SELECT faculty.fid, faculty.fname
+FROM faculty
+WHERE NOT EXISTS (
+    SELECT course.cid FROM course, teaches
+    WHERE faculty.fid = teaches.fid AND course.cid = teaches.cid
+    AND course.did != faculty.did
+) AND EXISTS (
+    SELECT course.cid FROM course, teaches
+    WHERE faculty.fid = teaches.fid AND course.cid = teaches.cid
+    AND course.did = faculty.did
+);
+
+-- 10
+SELECT faculty.fid, faculty.fname
+FROM faculty
+WHERE NOT EXISTS (
+    SELECT course.cid FROM course, teaches
+    WHERE faculty.fid = teaches.fid AND course.cid = teaches.cid
+    AND course.did = faculty.did
+) AND EXISTS (
+    SELECT course.cid FROM course, teaches
+    WHERE faculty.fid = teaches.fid AND course.cid = teaches.cid
+    AND course.did != faculty.did
+);
 
 -- 11
 WITH student_info (avg_age, cid) AS (
@@ -109,11 +156,71 @@ SELECT st.cid, c.cname
 FROM student_info st, course c 
 WHERE st.cid = c.cid AND st.n_students = (SELECT MAX(n_students) FROM student_info);
 
--- 14 cHECK IF IT MAKES SENSE
-WITH student_info AS (
-    SELECT cid, COUNT(*) n_students FROM marks WHERE marks = 70 GROUP BY cid
-)
-SELECT st.cid, c.cname FROM student_info st, course c 
-WHERE st.cid = c.cid AND st.n_students IN (SELECT COUNT(*) n_students FROM marks GROUP BY cid);
+-- 14 
+-- WITH student_info AS (
+--     SELECT cid, COUNT(*) n_students FROM marks WHERE marks = 70 GROUP BY cid
+-- )
+-- SELECT st.cid, c.cname FROM student_info st, course c 
+-- WHERE st.cid = c.cid AND st.n_students IN (SELECT COUNT(*) n_students FROM marks GROUP BY cid);
+
+SELECT marks.cid, course.cname
+FROM marks, course
+WHERE marks.cid = course.cid
+GROUP BY cid
+HAVING MIN(marks) = 70 AND MAX(marks) = 70;
+
+-- 15
+SELECT student.sid, student.sname, AVG(marks) AS avg_marks, 
+CASE
+    WHEN AVG(marks) >= 80 THEN "AA"
+    WHEN AVG(marks) >= 60 THEN "AB"
+    WHEN AVG(marks) >= 40 THEN "BB"
+    WHEN AVG(marks) >= 20 THEN "BC"
+    ELSE "F"
+END AS grade
+FROM marks, student
+WHERE student.sid = marks.sid
+GROUP BY sid;
+
+-- 16
+DELIMITER //
+CREATE FUNCTION get_instructor_count(dept_name VARCHAR(255))
+RETURNS INT DETERMINISTIC
+BEGIN
+  DECLARE instructor_count INT;
+  SELECT COUNT(*) INTO instructor_count
+  FROM faculty f, department d
+  WHERE f.did = d.did 
+  AND d.dname = dept_name;
+  RETURN instructor_count;
+END //
+DELIMITER ; 
+
+-- 17
+SELECT * FROM department WHERE get_instructor_count(dname) >= 2;
+
+-- 18
+DELIMITER //
+CREATE FUNCTION get_faculty_count_by_salary(salary NUMERIC(8, 2)) 
+RETURNS INT DETERMINISTIC
+BEGIN
+    DECLARE faculty_count INT;
+    SELECT COUNT(*) INTO faculty_count FROM faculty
+        WHERE faculty.salary = salary;
+    RETURN faculty_count;
+END //
+DELIMITER ;
+
+-- 19
+SELECT get_faculty_count_by_salary(100) AS salary_faculty_count;
+
+-- 20
+SELECT * FROM faculty WHERE get_faculty_count_by_salary(salary) >= 2;
+
+
+
+
+
+
 
 
